eaaa41da9fd5f28ce90d4416422b25068ea4e8ea
emlyjwllms <emlyjwllms@gmail.com>
2023-12-03 17:44:31 -0500

    updated plots

--- lorenz-plot.py
+++ lorenz-plot.py
@@ -50,9 +50,9 @@ dg_lorenz_0 = np.load('dg_lorenz_dt100_p0.npz')
 xh_0 = dg_lorenz_0['xh']
 cs_0 = dg_lorenz_0['cs']
 
-# dg_lorenz = np.load('dg_lorenz_dt1000_p3.npz')
-# xh = dg_lorenz['xh']
-# cs = dg_lorenz['cs']
+dg_lorenz = np.load('dg_lorenz_dt1000_p3.npz')
+xh = dg_lorenz['xh']
+cs = dg_lorenz['cs']
 
 fig1 = plt.figure(figsize=(7,10))
 ax1 = fig1.add_subplot(3,1,1)
@@ -106,58 +106,59 @@ ax1 = fig1.add_subplot(3,1,1)
 ax2 = fig1.add_subplot(3,1,2)
 ax3 = fig1.add_subplot(3,1,3)
 
-# integrate across elements
-for j in range(1,N): # loop across I_j's
-    t0 = t[j-1]
-    tf = t[j]
-    c = cs_0[:,:,j] # solve residual function above
-    ax1.scatter(dt*xi/2 + (t0+tf)/2, phi @ c[0,:])
-    ax2.scatter(dt*xi/2 + (t0+tf)/2, phi @ c[1,:])
-    ax3.scatter(dt*xi/2 + (t0+tf)/2, phi @ c[2,:])
+# # integrate across elements
+# for j in range(1,N): # loop across I_j's
+#     t0 = t[j-1]
+#     tf = t[j]
+#     c = cs_0[:,:,j] # solve residual function above
+#     ax1.scatter(dt*xi/2 + (t0+tf)/2, phi @ c[0,:])
+#     ax2.scatter(dt*xi/2 + (t0+tf)/2, phi @ c[1,:])
+#     ax3.scatter(dt*xi/2 + (t0+tf)/2, phi @ c[2,:])
 
-ax1.scatter(t,xh_0[0,:],label=r"$x$")
-ax2.scatter(t,xh_0[1,:],label=r"$y$")
-ax3.scatter(t,xh_0[2,:],label=r"$z$")
-ax3.set_xlabel(r"$t$")
-#plt.legend()
-plt.grid()
-#plt.savefig('plots/dg-lorenz.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
-plt.show()
+# ax1.scatter(t,xh_0[0,:],label=r"$x$")
+# ax2.scatter(t,xh_0[1,:],label=r"$y$")
+# ax3.scatter(t,xh_0[2,:],label=r"$z$")
+# ax3.set_xlabel(r"$t$")
+# #plt.legend()
+# plt.grid()
+# #plt.savefig('plots/dg-lorenz.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
+# plt.show()
 
 
-fig1 = plt.figure(figsize=(7,10))
-ax1 = fig1.add_subplot(3,1,1)
-ax1.plot(t,xh_0[0,:],label="DG, p = 0")
-ax1.plot(t,x_BE[:,0],'k--',label="BE")
-ax1.set_ylabel(r"$x$")
-ax1.legend()
-ax1.grid()
+# fig1 = plt.figure(figsize=(7,10))
+# ax1 = fig1.add_subplot(3,1,1)
+# ax1.plot(t,xh_0[0,:],label="DG, p = 0")
+# ax1.plot(t,x_BE[:,0],'k--',label="BE")
+# ax1.set_ylabel(r"$x$")
+# ax1.legend()
+# ax1.grid()
 
-ax2 = fig1.add_subplot(3,1,2)
-ax2.plot(t,xh_0[1,:],label="DG, p = 0")
-ax2.plot(t,x_BE[:,1],'k--',label="BE")
-ax2.set_ylabel(r"$y$")
-#ax2.legend()
-ax2.grid()
+# ax2 = fig1.add_subplot(3,1,2)
+# ax2.plot(t,xh_0[1,:],label="DG, p = 0")
+# ax2.plot(t,x_BE[:,1],'k--',label="BE")
+# ax2.set_ylabel(r"$y$")
+# #ax2.legend()
+# ax2.grid()
 
-ax3 = fig1.add_subplot(3,1,3)
-ax3.plot(t,xh_0[2,:],label="DG, p = 0")
-ax3.plot(t,x_BE[:,2],'k--',label="BE")
-ax3.set_ylabel(r"$z$")
-#ax3.legend()
-ax3.set_xlabel(r"$t$")
-ax3.grid()
+# ax3 = fig1.add_subplot(3,1,3)
+# ax3.plot(t,xh_0[2,:],label="DG, p = 0")
+# ax3.plot(t,x_BE[:,2],'k--',label="BE")
+# ax3.set_ylabel(r"$z$")
+# #ax3.legend()
+# ax3.set_xlabel(r"$t$")
+# ax3.grid()
 
-#plt.savefig('plots/dg-be-lorenz.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
-plt.show()
+# #plt.savefig('plots/dg-be-lorenz.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
+# plt.show()
 
 fig = plt.figure(figsize=(7,7))
 ax1 = fig.add_subplot(1,1,1,projection='3d')
-ax1.plot(*xh_0,label="DG")
+ax1.plot(*xh,label=r"$\mathbf{x}")
 ax1.set_xlabel(r"$x$")
 ax1.set_ylabel(r"$y$")
 ax1.set_zlabel(r"$z$")
 ax1.legend()
 
-#plt.savefig('plots/dg-lorenz-3d.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
+# plt.savefig('plots/dg-lorenz-3d.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
+plt.savefig('states-3d.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
 plt.show()
--- tangent-model-plot.py
+++ tangent-model-plot.py
@@ -42,93 +42,122 @@ def plegendre(x,porder):
 
 tilde = False
 
-dg_lorenz = np.load('dg_lorenz_dt100_stoch_p2.npz')
+dg_lorenz = np.load('dg_lorenz_dt100_p2.npz')
 xh = dg_lorenz['xh']
 xh_tilde = dg_lorenz['xh_tilde']
 t = dg_lorenz['t']
 
-dg_lorenz_filtered = np.load('dg_lorenz_dt100_p2_stoch.npz')
-xhbar = dg_lorenz_filtered['xhbar']
-xhbar_tilde = dg_lorenz_filtered['xhbar_tilde']
-
-N = len(t)
-dt = t[1] - t[0]
-porder = 0
-
-xi, w = scipy.special.roots_legendre(porder+1)
-phi, dphi = plegendre(xi,porder)
+# dg_lorenz_filtered = np.load('dg_lorenz_dt100_p2_stoch.npz')
+# xhbar = dg_lorenz_filtered['xhbar']
+# xhbar_tilde = dg_lorenz_filtered['xhbar_tilde']
 
 fig1 = plt.figure(figsize=(10,2))
-
 ax2 = fig1.add_subplot(1,3,1)
 ax2y = fig1.add_subplot(1,3,2)
 ax2z = fig1.add_subplot(1,3,3)
 
-# ax3 = fig1.add_subplot(3,2,2)
-# ax3y = fig1.add_subplot(3,2,4)
-# ax3z = fig1.add_subplot(3,2,6)
-
-ax2.plot(t,np.log(xh_tilde[0,:]),color='C0')
-ax2y.plot(t,np.log(xh_tilde[1,:]),color='C1')
-ax2z.plot(t,np.log(xh_tilde[2,:]),color='C2')
-ax2.set_ylabel(r"$\log(\tilde{\mathbf{x}})$")
-ax2.set_xlabel(r"$t$")
-ax2y.set_xlabel(r"$t$")
-ax2z.set_xlabel(r"$t$")
-# ax2.set_yscale("log")
-# ax2y.set_yscale("log")
-# ax2z.set_yscale("log")
-# ax2.set_ylim(-300,300)
-# ax2y.set_ylim(-300,300)
-# ax2z.set_ylim(-300,300)
-
-# ax3.plot(t,xhbar_tilde[0,:],color='C0')
-# ax3y.plot(t,xhbar_tilde[1,:],color='C1')
-# ax3z.plot(t,xhbar_tilde[2,:],color='C2')
-# ax3.set_title(r"$\tilde{\overline{\mathbf{x}}}$")
-# ax3z.set_xlabel(r"$t$")
-# ax3.set_ylim(-300,300)
-# ax3y.set_ylim(-300,300)
-# ax3z.set_ylim(-300,300)
-
-
-#plt.savefig('plots/dg-lorenz-tangent-linear.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
+ax2.plot(t,(xh_tilde[0,:]),color='C0')
+ax2y.plot(t,(xh_tilde[1,:]),color='C1')
+ax2z.plot(t,(xh_tilde[2,:]),color='C2')
+ax2.set_ylabel(r"$(\tilde{\mathbf{x}})$")
+ax2.set_ylim(-300,300)
+ax2y.set_ylim(-300,300)
+ax2z.set_ylim(-300,300)
 plt.show()
 
-if 0:
-    # Lyapunov covariant vectors
+fig = plt.figure(figsize=(7,7))
+ax1 = fig.add_subplot(1,1,1,projection='3d')
+ax1.plot(*xh_tilde,label=r"$\tilde{\mathbf{x}}$")
+ax1.set_xlabel(r"$\tilde{x}$")
+ax1.set_ylabel(r"$\tilde{y}$")
+ax1.set_zlabel(r"$\tilde{z}$")
+ax1.set_xlim(-300,300)
+ax1.set_ylim(-300,300)
+ax1.set_zlim(-300,300)
+#ax1.legend()
 
-    lambda_ = 0.95
+plt.savefig('unstable-3d.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
+plt.show()
 
-    dg_lorenz_perturbed = np.load('dg_lorenz_dt100_p2_perturbed.npz')
-    xh_perturbed = dg_lorenz_perturbed['xh']
-    cs_perturbed = dg_lorenz_perturbed['cs']
 
-    fig0 = plt.figure(figsize=(12,4))
-    ax1 = fig0.add_subplot(1,2,1)
-    ax1.semilogy(t,np.sqrt(np.abs(xh_perturbed[0,:]-xh[0,:])**2),'C0')
-    ax1.semilogy(t,np.sqrt(np.abs(xh_perturbed[1,:]-xh[1,:])**2),'C1')
-    ax1.semilogy(t,np.sqrt(np.abs(xh_perturbed[2,:]-xh[2,:])**2),'C2')
-    ax1.semilogy(t,10**-1.5*np.exp(lambda_*t),'k--',label=r"$\exp(\lambda t)$ with $\lambda$ = " + str(lambda_))
-    ax1.legend(loc='lower right')
-    ax1.set_xlabel(r"$t$")
-    ax1.set_ylabel(r"$||\mathbf{x}_h^p - \mathbf{x}_h||$")
-    ax1.set_ylim(10**-6,10**2)
+# N = len(t)
+# dt = t[1] - t[0]
+# porder = 0
 
-    phi = xh_tilde*np.exp(-lambda_*t)
+# xi, w = scipy.special.roots_legendre(porder+1)
+# phi, dphi = plegendre(xi,porder)
 
-    ax2 = fig0.add_subplot(1,2,2)
+# fig1 = plt.figure(figsize=(10,2))
 
-    ax2.plot(t,phi[0,:],label=r"$x$")
-    ax2.plot(t,phi[1,:],label=r"$y$")
-    ax2.plot(t,phi[2,:],label=r"$z$")
-    ax2.set_ylabel(r"$\mathbf{\phi}$")
-    ax2.set_xlabel(r"$t$")
-    ax2.legend()
-    ax2.set_ylim(-50,50)
+# ax2 = fig1.add_subplot(1,3,1)
+# ax2y = fig1.add_subplot(1,3,2)
+# ax2z = fig1.add_subplot(1,3,3)
 
-    #plt.savefig('plots/dg-lorenz-tangent-linear-lyapunov.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
-    plt.show()
+# # ax3 = fig1.add_subplot(3,2,2)
+# # ax3y = fig1.add_subplot(3,2,4)
+# # ax3z = fig1.add_subplot(3,2,6)
+
+# ax2.plot(t,np.log(xh_tilde[0,:]),color='C0')
+# ax2y.plot(t,np.log(xh_tilde[1,:]),color='C1')
+# ax2z.plot(t,np.log(xh_tilde[2,:]),color='C2')
+# ax2.set_ylabel(r"$\log(\tilde{\mathbf{x}})$")
+# ax2.set_xlabel(r"$t$")
+# ax2y.set_xlabel(r"$t$")
+# ax2z.set_xlabel(r"$t$")
+# # ax2.set_yscale("log")
+# # ax2y.set_yscale("log")
+# # ax2z.set_yscale("log")
+# # ax2.set_ylim(-300,300)
+# # ax2y.set_ylim(-300,300)
+# # ax2z.set_ylim(-300,300)
+
+# # ax3.plot(t,xhbar_tilde[0,:],color='C0')
+# # ax3y.plot(t,xhbar_tilde[1,:],color='C1')
+# # ax3z.plot(t,xhbar_tilde[2,:],color='C2')
+# # ax3.set_title(r"$\tilde{\overline{\mathbf{x}}}$")
+# # ax3z.set_xlabel(r"$t$")
+# # ax3.set_ylim(-300,300)
+# # ax3y.set_ylim(-300,300)
+# # ax3z.set_ylim(-300,300)
+
+
+# #plt.savefig('plots/dg-lorenz-tangent-linear.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
+# plt.show()
+
+# if 0:
+#     # Lyapunov covariant vectors
+
+#     lambda_ = 0.95
+
+#     dg_lorenz_perturbed = np.load('dg_lorenz_dt100_p2_perturbed.npz')
+#     xh_perturbed = dg_lorenz_perturbed['xh']
+#     cs_perturbed = dg_lorenz_perturbed['cs']
+
+#     fig0 = plt.figure(figsize=(12,4))
+#     ax1 = fig0.add_subplot(1,2,1)
+#     ax1.semilogy(t,np.sqrt(np.abs(xh_perturbed[0,:]-xh[0,:])**2),'C0')
+#     ax1.semilogy(t,np.sqrt(np.abs(xh_perturbed[1,:]-xh[1,:])**2),'C1')
+#     ax1.semilogy(t,np.sqrt(np.abs(xh_perturbed[2,:]-xh[2,:])**2),'C2')
+#     ax1.semilogy(t,10**-1.5*np.exp(lambda_*t),'k--',label=r"$\exp(\lambda t)$ with $\lambda$ = " + str(lambda_))
+#     ax1.legend(loc='lower right')
+#     ax1.set_xlabel(r"$t$")
+#     ax1.set_ylabel(r"$||\mathbf{x}_h^p - \mathbf{x}_h||$")
+#     ax1.set_ylim(10**-6,10**2)
+
+#     phi = xh_tilde*np.exp(-lambda_*t)
+
+#     ax2 = fig0.add_subplot(1,2,2)
+
+#     ax2.plot(t,phi[0,:],label=r"$x$")
+#     ax2.plot(t,phi[1,:],label=r"$y$")
+#     ax2.plot(t,phi[2,:],label=r"$z$")
+#     ax2.set_ylabel(r"$\mathbf{\phi}$")
+#     ax2.set_xlabel(r"$t$")
+#     ax2.legend()
+#     ax2.set_ylim(-50,50)
+
+#     #plt.savefig('plots/dg-lorenz-tangent-linear-lyapunov.png',dpi=300,format='png',transparent=True,bbox_inches='tight')
+#     plt.show()
 
 
 
